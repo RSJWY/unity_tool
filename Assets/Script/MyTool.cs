8489ff8f636769d5abcbd3acb3582c8b399a3f20
若时间无垠_笔记本 <1446176202@qq.com>
2023-04-08 22:07:49 +0800

    工具类增加对Transform的设置

--- Assets/Script/MyTool.cs
+++ Assets/Script/MyTool.cs
@@ -135,29 +135,96 @@ public class MyTool
         return string.Format("{0:D2}:{1:D2}:{2:D2} ", 
             DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
     }
-#if UNITY_EDITOR
+
+
+
+    #region Transform相关设置
     /// <summary>
-    /// 检查路径下是否有重命名的文件，并重新指定一个没有任何冲突的名字
-    /// 规则为：类型名字+_+编号+后缀
+    /// 快速设置Transform位置信息
+    /// <para>默认位置与旋转皆为0，scale为1</para>
     /// </summary>
-    /// <param name="_url">路径</param>
-    /// <param name="_suffix">后缀名，默认为 "*.asset" </param>
-    /// <returns></returns>
-    public static string TryGetName<T>(string _url, string _suffix = ".asset")
+    /// <param name="islocal"></param>
+    /// <param name="target"></param>
+    /// <param name="position"></param>
+    /// <param name="EulerAngle"></param>
+    /// <param name="localscale"></param>
+    public void SetNewTransformInfo(bool islocal, Transform target)
     {
-        //新建初始参数
-        string str = "";
-        int index = 0;
-        UnityEngine.Object obj = null;
-        do
+        //判断转换相对还是世界
+        if (islocal)
         {
-            str = _url + "/" + typeof(T).Name + "_" + index + _suffix;//拼接
-            obj = UnityEditor.AssetDatabase.LoadAssetAtPath(str, typeof(T));//尝试加载
-            index++;
-        } while (obj);//没有要找的，退出
-        return str;//文件名不冲突，返回名字
+            target.transform.localPosition = Vector3.zero;
+            target.transform.localEulerAngles = Vector3.zero;
+        }
+        else
+        {
+            target.transform.position =Vector3.zero;
+            target.transform.eulerAngles = Vector3.zero;
+        }
+        target.transform.localScale= Vector3.one;
     }
-#endif
+
+    /// <summary>
+    /// 快速设置Transform位置信息
+    /// <para>default 默认为 Vector3(0,0,0)</para>
+    /// </summary>
+    /// <param name="islocal">世界坐标还是相对坐标，</param>
+    /// <param name="target">目标的Transform组件</param>
+    /// <param name="position">POS值</param>
+    /// <param name="EulerAngle">Euler值</param>
+    /// <param name="Rotation">Euler值</param>
+    /// <param name="localscale"></param>
+    public void SetNewTransformInfo(bool islocal, Transform target,
+        Vector3 position=default, Vector3 EulerAngle = default, Vector3 localscale = default)
+    {
+        //判断转换相对还是世界
+        if (islocal)
+        {
+            target.transform.localPosition = position == default ? Vector3.zero : position;
+            target.transform.localEulerAngles = EulerAngle == default ? Vector3.zero : EulerAngle;
+        }
+        else
+        {
+            target.transform.position = position == default ? Vector3.zero : position;
+            target.transform.eulerAngles = EulerAngle == default ? Vector3.zero : EulerAngle;
+        }
+        target.transform.localScale = localscale == default ? Vector3.one : localscale;
+    }
+
+    /// <summary>
+    /// 快速设置Transform位置信息
+    /// <para>default 默认为 Vector3(0,0,0)</para>
+    /// <para>如果四元数值为空，则赋值操作将由欧拉角转为四元数进行操作</para>
+    /// </summary>
+    /// <param name="islocal">世界坐标还是相对坐标，</param>
+    /// <param name="target">目标的Transform组件</param>
+    /// <param name="position">POS值</param>
+    /// <param name="Rotation">四元数值</param>
+    /// <param name="Rotation">Euler值</param>
+    /// <param name="localscale"></param>
+    public void SetNewTransformInfo(bool islocal, Transform target,
+        Vector3 position = default, Quaternion Rotation = default, Vector3 localscale = default)
+    {
+        //判断四元数信息
+        if (Rotation == default)
+        {
+            Rotation = Quaternion.Euler(Vector3.zero);
+        }
+
+        //判断转换相对还是世界
+        if (islocal)
+        {
+            target.transform.localPosition = position == default ? Vector3.zero : position;
+            target.transform.localRotation = Rotation;
+        }
+        else
+        {
+            target.transform.position = position == default ? Vector3.zero : position;
+            target.transform.rotation = Rotation;
+        }
+        target.transform.localScale = localscale == default ? Vector3.zero : localscale;
+    }
+    #endregion
 
     #region 废弃
     //废弃
@@ -232,5 +299,31 @@ public class MyTool
         asy.allowSceneActivation = true;
     }
 
-    #endregion 
+    #endregion
+
+
+
+#if UNITY_EDITOR
+    /// <summary>
+    /// 检查路径下是否有重命名的文件，并重新指定一个没有任何冲突的名字
+    /// 规则为：类型名字+_+编号+后缀
+    /// </summary>
+    /// <param name="_url">路径</param>
+    /// <param name="_suffix">后缀名，默认为 "*.asset" </param>
+    /// <returns></returns>
+    public static string TryGetName<T>(string _url, string _suffix = ".asset")
+    {
+        //新建初始参数
+        string str = "";
+        int index = 0;
+        UnityEngine.Object obj = null;
+        do
+        {
+            str = _url + "/" + typeof(T).Name + "_" + index + _suffix;//拼接
+            obj = UnityEditor.AssetDatabase.LoadAssetAtPath(str, typeof(T));//尝试加载
+            index++;
+        } while (obj);//没有要找的，退出
+        return str;//文件名不冲突，返回名字
+    }
+#endif
 }
