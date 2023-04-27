e84a6c9ce0f7fa70c80f5da53b53f28991e33a7d
若时间无垠_笔记本 <1446176202@qq.com>
2023-04-27 20:35:23 +0800

    文件结构目录重构，因代码特殊性，不再新建项目进行调试

---
+++ 部分常用函数/MyClient.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;//线程
+using System;
+
+public class MyClient : MonoBehaviour
+{
+    public InputField input;
+    public InputField port_in;
+    public Button btn;
+    public Text t;
+
+    public InputField send_str;
+    public Button send_btn;
+
+    string ip;
+    int port = 8080;
+    TcpClient client;
+    NetworkStream stream;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn.onClick.AddListener(ClickEvent);
+        send_btn.onClick.AddListener(SendEvent);
+    }
+    /// <summary>
+    /// 发送信息
+    /// </summary>
+    private void SendEvent()
+    {
+        string _str = send_str.text;
+        try
+        {
+            //转换成流
+            byte[] _b = Encoding.Unicode.GetBytes(_str);
+            stream.Write(_b, 0, _b.Length);
+        }
+        catch (Exception e)
+        {
+
+            t.text = "\n" + e.ToString();
+        }
+    }
+    /// <summary>
+    /// 连接服务器
+    /// </summary>
+    void ClickEvent()
+    {
+        client = new TcpClient();
+        ip = input.text;
+        port = int.Parse(port_in.text);
+
+        try
+        {
+            //尝试链接
+            client.Connect(IPAddress.Parse(ip), port);
+            t.text = "连接成功";
+            stream = client.GetStream();//获取自身的流
+        }
+        catch (Exception e)
+        {
+            t.text += "\n" + e.ToString();
+        }
+        //监听服务器线程
+        client = new TcpClient();//TCP通讯
+        ip = input.text;//输入
+        Thread p = new Thread(MyStart);//开线程
+        p.Start();
+    }
+
+    private void MyStart(object obj)
+    {
+        do
+        {
+            try
+            {
+                int size = 1024;//长度
+                byte[] readBuffer = new byte[size];
+                int reader = stream.Read(readBuffer, 0, size);//将转换的信息流存入reader中
+                if (reader == 0)
+                {
+                    t.text += "\n" + "服务端断开连接";
+                    break;
+                }//发送信息
+                string str = Encoding.Unicode.GetString(readBuffer, 0, reader);
+                if (str.Length > 0)
+                {
+                    t.text += "\n" + str;
+                }
+
+            }
+            catch (Exception e)
+            {
+                t.text += "\n" + e.ToString();
+
+            }
+
+        } while (true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 部分常用函数/MyService.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;//线程
+using System;
+
+/// <summary>
+/// 服务器代码
+/// </summary>
+public class MyService : MonoBehaviour
+{
+
+    public InputField input;
+    public InputField port_in;
+    public Button btn;
+    public Text t;
+
+
+    public InputField send_str;
+    public Button send_btn;
+
+    string ip;
+    int port = 8080;
+    TcpClient client;
+    NetworkStream stream;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn.onClick.AddListener(() =>
+        {
+            //记录输入的ip
+            ip = input.text;
+            port = int.Parse(port_in.text);
+            //开启线程
+            Thread p = new Thread(MyStart);
+            p.Start();
+        });
+        send_btn.onClick.AddListener(SendEvent);
+
+    }
+
+    private void SendEvent()
+    {
+        string str = send_str.text;
+        try
+        {
+            byte[] b = Encoding.Unicode.GetBytes(str);
+            stream.Write(b, 0, b.Length);
+
+
+        }
+        catch (Exception e)
+        {
+            t.text += "\n" + e.ToString();
+
+        }
+    }
+
+    //新线程监听任务
+    private void MyStart()
+    {
+        //根据ip和端口，开始监听
+        TcpListener listener = new TcpListener(IPAddress.Parse(ip), port);
+        listener.Start();
+        t.text = "开启服务器";
+        client = listener.AcceptTcpClient();//客户端链接
+        stream = client.GetStream();//获取客户端流
+        t.text += "\n 客户端已连接";
+
+        //循环监听
+        do
+        {
+            try
+            {
+                int size = 1024;//占位
+                byte[] b = new byte[size];//数组创建
+                int reader = stream.Read(b,0,size);//读取
+                //如果读到的数据长度为0，链接断开
+                if (reader==0)
+                {
+                    t.text += "\n 客户端断开";
+                    break;
+                }
+                //获取接收到的流
+                string str = Encoding.Unicode.GetString(b,0,reader);
+                //长度够，打印输出
+                if (str.Length>0)
+                {
+                    t.text += "\n"+str;
+                }
+            }
+            catch (Exception e)
+            {
+
+                t.text += "\n"+e.ToString();
+            }
+        } while (true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
