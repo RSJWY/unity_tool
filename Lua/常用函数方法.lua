8256728c3584f1023a5adfa04b29b2e84c016c4d
若时间无垠_笔记本 <1446176202@qq.com>
2023-06-02 17:40:43 +0800

    增加Lua

---
+++ Lua/常用函数方法.lua
@@ -0,0 +1,18 @@
+---[ Table 最大值以及返回的索引 ]
+function Table_maxn(t)
+  local mn=nil;
+  for k, v in pairs(t) do
+    if(mn==nil) 
+    then
+      mn=v
+    end
+    if mn < v 
+    then
+      mn = v
+    end
+  end
+  return mn
+end
+tbl = {[1] = 2, [2] = 6, [3] = 34, [26] =5}
+print("tbl 最大值：", table_maxn(tbl))
+print("tbl 长度 ", #tbl)
